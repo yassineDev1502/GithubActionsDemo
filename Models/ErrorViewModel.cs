2017fd60b772842b70bdfc87119a12c819fe2665
yassineelkharmoudi <yassine.elkharmoudi.cons@autorita-trasporti.it>
2026-07-23 10:27:32 +0200

    Add ASP.NET project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GithubActionsDemo.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
